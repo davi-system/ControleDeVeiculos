68bb1feea0270926811ba13cc51c72b73daccb70
Davi <davialexandre.contato@hotmail.com>
2023-09-05 16:51:00 -0300

    Add Diretorios, Templates e configurando novas Rotas

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, veiculo, motorista
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('veiculo', veiculo, name='veiculo'),
+    path('motorista', motorista, name='motorista')
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def veiculo(request):
+    return render(request, 'veiculo.html')
+
+
+def motorista(request):
+    return render(request, 'motorista.html')
